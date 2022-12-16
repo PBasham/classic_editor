5c57dbbb6dab695aa6e3a94615ca25c34bea3149
patrickbasham <PatrickTBasham@gmail.com>
2022-12-16 15:41:12 -0500

    first paragraph set up and cannot be deleted

--- js/index.js
+++ js/index.js
@@ -2,6 +2,32 @@ console.log("JS is connected!")
 
 $(document).ready(() => {
     console.log("Document Loaded")
-    
+    let temp = `<br data-cke-filler="true"></br>`
+
+    let controleBar = $("#controle-bar")
+    let editor = $("#editor")
+    let firstParagraph = $("#starter-paragraph")
+    // set these equal to the buttons. and track if the button is selected or not.
+    let isBold = false
+    let isItalic = false
+    let isBulletPoints = false
+    let isNumberList = false
+
+
+
+    /*  --------------------------------------------------
+            Event Listeners
+    --------------------------------------------------  */
+    // listen for backspace press.
+    $("#editor").on("keyup", ((e) => {
+        if (e.key === "Backspace") {
+            if ($("#editor #starter-paragraph").length === 0) {
+                editor.append(firstParagraph)
+            }
+        }
+    }))
 
 })
+
+// console.log("creating new first paragraph")
+// editor.insertBefore(firstParagraph, editor.firstChild)
